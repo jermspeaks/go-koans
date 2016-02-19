3f8a343c2afbf56ccd352b109ceb9299e7633b55
Jeremy <jermspeaks@gmail.com>
2016-02-18 23:47:34 -0800

    teaching you about errors

--- setup_koans_test.go
+++ setup_koans_test.go
@@ -42,7 +42,7 @@ func TestKoans(t *testing.T) {
 	aboutAllocation()
 	aboutChannels()
 	aboutConcurrency()
-	// aboutPanics()
+	aboutPanics()
 
 	fmt.Printf("\n%c[32;1mYou won life. Good job.%c[0m\n\n", 27, 27)
 }
